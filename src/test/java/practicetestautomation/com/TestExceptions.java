dfd67a9acbfc9a62c9895f83d9640843c45bb90c
neIIIexog777 <neIIIexog@gmail.com>
2026-02-05 10:20:51 +0300

    change gradle

--- src/test/java/practicetestautomation/com/TestExceptions.java
+++ src/test/java/practicetestautomation/com/TestExceptions.java
@@ -1,7 +1,5 @@
 package practicetestautomation.com;
 
-import org.junit.jupiter.api.AfterEach;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
